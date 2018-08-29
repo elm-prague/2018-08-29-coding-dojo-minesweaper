5b18543fbaf73f9d10487ce9caba50b77edff3c1
Tomáš Horáček <tomas.horacek@gmail.com>
2018-08-29 20:30:42 +0200

    Final state

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ The same field including the hint numbers described above would look like this:
 
 You should write a program that takes input as follows:
 
-The input will consist of an arbitrary number of fields. The first line of each field contains two integers n and m (0 \< n,m \<= 100) which stands for the number of lines and columns of the field respectively. The next n lines contains exactly m characters and represent the field. Each safe square is represented by an “.” character (without the quotes) and each mine square is represented by an “\*” character (also without the quotes). The first field line where n = m = 0 represents the end of input and should not be processed.
+The input will consist of an arbitrary number of fields. The first line of each field contains two integers `n` and `m` (where `0 < n,m <= 100`) which stands for the number of lines and columns of the field respectively. The next `n` lines contains exactly m characters and represent the field. Each safe square is represented by an `.` character and each mine square is represented by an `*` character. The first field line where `n = m = 0` represents the end of input and should not be processed.
 
 Your program should produce output as follows:
 
@@ -40,7 +40,7 @@ For each field, you must print the following message in a line alone:
 Field #x:
 ```
 
-Where `x` stands for the number of the field (starting from 1). The next n lines should contain the field with the “.” characters replaced by the number of adjacent mines to that square. There must be an empty line between field outputs.
+Where `x` stands for the number of the field (starting from 1). The next `n` lines should contain the field with the `.` characters replaced by the number of adjacent mines to that square. There must be an empty line between field outputs.
 
 ### Clues
 
--- tests/Example.elm
+++ tests/Example.elm
@@ -1,16 +1,131 @@
 module Example exposing (suite)
 
+import Array
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Test exposing (..)
 
 
+fourByFourInput =
+    [ "...."
+    , "...."
+    , "..**"
+    , "..*."
+    ]
+
+
+oneByoneInput =
+    [ "*" ]
+
+
+oneBytwoInput =
+    [ "**" ]
+
+
+charAt : List String -> Int -> Int -> String
+charAt field x y =
+    let
+        elemAt index list =
+            list |> Array.fromList |> Array.get index
+    in
+    field
+        |> elemAt y
+        |> Maybe.map (String.slice x (x + 1))
+        |> Maybe.withDefault "."
+
+
+getOutput : List String -> Int -> Int -> String
+getOutput field x y =
+    case charAt field x y of
+        "*" ->
+            "*"
+
+        _ ->
+            getNeighbours x y
+                |> List.map (\( a, b ) -> charAt field a b)
+                |> List.filter (\a -> a == "*")
+                |> List.length
+                |> String.fromInt
+
+
+neighbours =
+    [ ( 0, -1 ), ( 1, -1 ), ( 1, 0 ), ( 1, 1 ), ( 0, 1 ), ( -1, 1 ), ( -1, 0 ), ( -1, -1 ) ]
+
+
+getNeighbours x y =
+    List.map (\( dx, dy ) -> ( x + dx, y + dy )) neighbours
+
+
 suite : Test
 suite =
-    describe "Some Suite"
-        [ test "String.toLower a known string" <|
-            \_ ->
-                Expect.equal
-                    "abc"
-                    (String.toLower "ABC")
+    describe "Minesweeper"
+        [ describe "getOutput"
+            [ test "* for (2,2) in 4x4 input" <|
+                \_ ->
+                    Expect.equal
+                        "*"
+                        (getOutput fourByFourInput 2 2)
+            , testOutputOnXY "4x4"
+                fourByFourInput
+                [ ( 0, 0, "0" )
+                , ( 1, 1, "1" )
+                , ( 1, 0, "0" )
+                , ( 2, 1, "2" )
+                , ( 2, 2, "*" )
+                , ( 3, 2, "*" )
+                , ( 3, 0, "0" )
+                , ( 2, 0, "0" )
+                , ( 0, 2, "0" )
+                ]
+            , testOutputOnXY "oneByoneInput"
+                oneByoneInput
+                [ ( 0, 0, "*" )
+                ]
+            , testOutputOnXY "oneBytwoInput"
+                oneBytwoInput
+                [ ( 0, 0, "*" )
+                ]
+            ]
+        , describe "getNeighbours"
+            [ test "(0,0)" <|
+                \_ ->
+                    Expect.equal
+                        [ ( 0, -1 ), ( 1, -1 ), ( 1, 0 ), ( 1, 1 ), ( 0, 1 ), ( -1, 1 ), ( -1, 0 ), ( -1, -1 ) ]
+                        (getNeighbours 0 0)
+            , test "(10,10)" <|
+                \_ ->
+                    Expect.equal
+                        [ ( 10, 9 )
+                        , ( 11, 9 )
+                        , ( 11, 10 )
+                        , ( 11, 11 )
+                        , ( 10, 11 )
+                        , ( 9, 11 )
+                        , ( 9, 10 )
+                        , ( 9, 9 )
+                        ]
+                        (getNeighbours 10 10)
+            ]
         ]
+
+
+testOutputOnXY fieldName field cases =
+    describe fieldName
+        (List.map
+            (\( x, y, output ) ->
+                test
+                    (output
+                        ++ " for ("
+                        ++ String.fromInt x
+                        ++ ","
+                        ++ String.fromInt y
+                        ++ ")"
+                    )
+                <|
+                    \_ ->
+                        Expect.equal
+                            output
+                            (getOutput field x y)
+            )
+            cases
+        )
